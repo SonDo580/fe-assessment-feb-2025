8efd72b33cd2b0876b620492cf0c607449576b1b
Son Do <dominhson580@gmail.com>
2025-02-11 23:02:16 +0700

    Handle input focus and dropdown showing

--- src/pages/SearchPage/components/Search/SearchInput.tsx
+++ src/pages/SearchPage/components/Search/SearchInput.tsx
@@ -1,25 +1,59 @@
 import classNames from "classnames";
-import { ChangeEvent, KeyboardEvent, useRef } from "react";
+import {
+  ChangeEvent,
+  KeyboardEvent,
+  Ref,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
+
+export interface SearchInputRef {
+  isFocused: boolean;
+  focus: () => void;
+  blur: () => void;
+}
 
 interface IProps {
   value: string;
   shouldShowDropdown: boolean;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   onSearch: () => void;
+  ref: Ref<SearchInputRef>;
 }
 
 function SearchInput({
+  ref,
   value,
   onChange,
   onSearch,
   shouldShowDropdown,
 }: IProps) {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+
+  useImperativeHandle(ref, () => ({
+    isFocused,
+    focus: () => {
+      inputRef.current?.focus();
+    },
+    blur: () => {
+      inputRef.current?.blur();
+    },
+  }));
 
   const onKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       onSearch();
     }
+  };
+
+  const onFocus = () => {
+    setIsFocused(true);
+  };
+
+  const onBlur = () => {
+    setIsFocused(false);
   };
 
   return (
@@ -29,6 +63,8 @@ function SearchInput({
       value={value}
       onChange={onChange}
       onKeyUp={onKeyUp}
+      onFocus={onFocus}
+      onBlur={onBlur}
       className={classNames(
         "w-full h-full py-2 pl-[22.98px] pr-8",
         "text-[#424242] text-base",
--- src/pages/SearchPage/components/Search/index.tsx
+++ src/pages/SearchPage/components/Search/index.tsx
@@ -1,24 +1,30 @@
 import classNames from "classnames";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 import ClearButton from "./ClearButton";
 import SearchButton from "./SearchButton";
-import SearchInput from "./SearchInput";
+import SearchInput, { SearchInputRef } from "./SearchInput";
 import SuggestionDropdown from "./SuggestionDropdown";
-import { useUpdateSearchParams } from "@/hooks/useUpdateSearchParams";
 
 function Search() {
-  const [keyword, setKeyword] = useState("");
-  const { updateSearchParam } = useUpdateSearchParams();
-  const shouldShowDropdown = !!keyword;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [keyword, setKeyword] = useState(searchParams.get("q") || "");
+  const searchInputRef = useRef<SearchInputRef>(null);
+  const shouldShowDropdown =
+    !!keyword && (searchInputRef.current?.isFocused as boolean);
 
   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setKeyword(e.target.value);
   };
 
   const onSearch = () => {
+    searchInputRef.current?.blur();
+
     if (keyword) {
-      updateSearchParam("q", keyword);
+      const newSearchParams = new URLSearchParams(searchParams);
+      newSearchParams.set("q", keyword);
+      setSearchParams(newSearchParams);
     }
   };
 
@@ -27,6 +33,7 @@ function Search() {
       <div className="flex gap-0">
         <div className="grow relative">
           <SearchInput
+            ref={searchInputRef}
             value={keyword}
             onChange={onInputChange}
             onSearch={onSearch}
