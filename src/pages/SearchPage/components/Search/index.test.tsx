1e8dc995def8daa6d5a0dcff669aea80874b841c
Son Do <dominhson580@gmail.com>
2025-02-16 18:04:30 +0700

    Test Search component - suggestion dropdown behavior

--- src/pages/SearchPage/components/Search/index.test.tsx
+++ src/pages/SearchPage/components/Search/index.test.tsx
@@ -6,6 +6,7 @@ import userEvent from "@testing-library/user-event";
 import { useQuery } from "@/hooks/useQuery";
 import { TSuggestionResults } from "@/types";
 import Search from ".";
+import { MIN_KEYWORD_LENGTH_FOR_SUGGESTIONS } from "@/constants";
 
 const SUGGESTION_DROPDOWN_TEST_ID = "SuggestionsDropdown";
 
@@ -33,6 +34,16 @@ const mockUseQueryReturnValue: UseQueryReturnType = {
   errorMsg: "",
   data: null,
 }; // default
+
+// Helper to generate random text with certain length
+const generateRandomText = (length: number) => {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let text = "";
+  for (let i = 0; i < length; i++) {
+    text += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+  }
+  return text;
+};
 
 describe("Search component", () => {
   let blurSpy: ReturnType<typeof vi.spyOn>;
@@ -87,7 +98,7 @@ describe("Search component", () => {
       expect(clearButton).toBeNull();
 
       // Focus on input but leave it empty
-      // -> clear should still not be rendered
+      // -> clear button should still not be rendered
       await userEvent.click(input);
       clearButton = screen.queryByLabelText(BTN_LABEL_TEXT);
       expect(clearButton).toBeNull();
@@ -110,10 +121,53 @@ describe("Search component", () => {
       clearButton = screen.getByLabelText(BTN_LABEL_TEXT);
 
       // Click clear button
-      // -> The input should be cleared but still focused
+      // -> input should be cleared but still focused
       await userEvent.click(clearButton);
       expect(input).toHaveValue("");
       expect(input).toHaveFocus();
+    });
+  });
+
+  describe("Search component - suggestions dropdown behavior", () => {
+    it("only render when input is focused and value is long enough", async () => {
+      render(<Search />);
+
+      const input = screen.getByRole("textbox");
+
+      // Dropdown should not be rendered initially
+      let dropdown = screen.queryByTestId(SUGGESTION_DROPDOWN_TEST_ID);
+      expect(dropdown).toBeNull();
+
+      // Focus on input but leave it empty
+      // -> dropdown should still not be rendered
+      await userEvent.click(input);
+      dropdown = screen.queryByTestId(SUGGESTION_DROPDOWN_TEST_ID);
+      expect(dropdown).toBeNull();
+
+      // Type text into input but not enough length
+      // -> dropdown should still not be rendered
+      const inputValue = generateRandomText(
+        MIN_KEYWORD_LENGTH_FOR_SUGGESTIONS - 1
+      );
+      await userEvent.type(input, inputValue);
+      dropdown = screen.queryByTestId(SUGGESTION_DROPDOWN_TEST_ID);
+      expect(dropdown).toBeNull();
+
+      // The input value becomes long enough
+      // -> dropdown should appear
+      await userEvent.type(input, "a");
+      dropdown = screen.getByTestId(SUGGESTION_DROPDOWN_TEST_ID);
+
+      // Blur input (value remains)
+      // -> dropdown should disappear
+      await userEvent.tab();
+      dropdown = screen.queryByTestId(SUGGESTION_DROPDOWN_TEST_ID);
+      expect(dropdown).toBeNull();
+
+      // Focus on input again
+      // -> dropdown should re-appear
+      await userEvent.click(input);
+      dropdown = screen.getByTestId(SUGGESTION_DROPDOWN_TEST_ID);
     });
   });
 });
