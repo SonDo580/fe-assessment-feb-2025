3e0c1e435c13672113b96b642ceacfff1a003065
Son Do <dominhson580@gmail.com>
2025-02-27 22:05:52 +0700

    Show suggestion dropdown when keyword length is greater than 2

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const MIN_KEYWORD_LENGTH_FOR_SUGGESTIONS = 2;
+export const MIN_KEYWORD_LENGTH_FOR_SUGGESTIONS = 3;
 export const NUMBER_OF_SUGGESTIONS_TO_DISPLAY = 6;
 
 export enum KeyboardKeys {
